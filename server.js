5e8507f34d2c54fb14b335ba886fd48cdc2586a6
Emanuel <limaemanuel773@gmail.com>
2025-05-08 15:49:28 -0300

    liberando acesso ao frontEnd

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const routes = require('./routes');
-
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
